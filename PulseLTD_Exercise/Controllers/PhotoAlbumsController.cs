01103f022b729c77d92c1c0a648309622d3f7ed7
Oscar Garcia <oscarantoniox10@gmail.com>
2022-03-22 20:16:34 -0700

    Deletion of unnecesary files.

--- PulseLTD_Exercise/Controllers/PhotoAlbumsController.cs
+++ PulseLTD_Exercise/Controllers/PhotoAlbumsController.cs
@@ -24,21 +24,6 @@ namespace PulseLTD_Exercise.Controllers
             return View(db.PhotoAlbums.ToList());
         }
 
-        // GET: PhotoAlbums/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PhotoAlbum photoAlbum = db.PhotoAlbums.Find(id);
-            if (photoAlbum == null)
-            {
-                return HttpNotFound();
-            }
-            return View(photoAlbum);
-        }
-
         [HttpPost]
         public ActionResult DownloadImages(int[] ids)
         {
@@ -84,11 +69,7 @@ namespace PulseLTD_Exercise.Controllers
             return View();
         }
 
-        // POST: PhotoAlbums/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        //[ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "ImageId,ImageTitle,ImageName,ImageText,ImageDateTime")] PhotoAlbum photoAlbum, HttpPostedFileBase file)
         {
             //HttpPostedFileBase photoFileBase =Request.Files[0];
@@ -108,18 +89,20 @@ namespace PulseLTD_Exercise.Controllers
         }
 
         [HttpPost]
-        public ActionResult PostImage(string title, string name/*, HttpPostedFileBase file*/)
+        public ActionResult PostImage(string title, string name)
         {
             if (Request.Files.Count > 0)
             {
                 Console.WriteLine(Request.Files.ToString());
                 HttpPostedFileBase postedFileBase = Request.Files[0];
                 WebImage image = new WebImage(postedFileBase.InputStream);
-                PhotoAlbum photoAlbum = new PhotoAlbum();
-                photoAlbum.ImageTitle = title;
-                photoAlbum.ImageName = name;
-                photoAlbum.ImageDateTime = DateTime.Now;
-                photoAlbum.ImageText = image.GetBytes();
+                PhotoAlbum photoAlbum = new PhotoAlbum
+                {
+                    ImageTitle = title,
+                    ImageName = name,
+                    ImageDateTime = DateTime.Now,
+                    ImageText = image.GetBytes()
+                };
                 if (ModelState.IsValid)
                 {
                     db.PhotoAlbums.Add(photoAlbum);
@@ -130,38 +113,6 @@ namespace PulseLTD_Exercise.Controllers
             }
             else return Json("No files to upload");
         }
-
-        // GET: PhotoAlbums/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PhotoAlbum photoAlbum = db.PhotoAlbums.Find(id);
-            if (photoAlbum == null)
-            {
-                return HttpNotFound();
-            }
-            return View(photoAlbum);
-        }
-
-        // POST: PhotoAlbums/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ImageId,ImageTitle,ImageName,ImageText,ImageDateTime")] PhotoAlbum photoAlbum)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(photoAlbum).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(photoAlbum);
-        }
-
 
         // GET: PhotoAlbums/Delete/5
         public ActionResult Delete(int? id)
